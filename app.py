ce3e5ded6bb73bb789e9a6e6bf1f0c21cb9a191a
Rojesh Babu Dahal <rojeshbabudahal@192.168.101.9>
2026-05-15 21:12:36 +0545

    Initial commit

---
+++ app.py
@@ -0,0 +1,62 @@
+import requests
+import json
+import os
+
+
+API_KEY = "e81e4bee64bc4c022185de5b9af6d536"
+FILE_NAME = "history.json"
+# city = "kathmandu"
+
+
+def search_weather():
+    
+    city_name = input("Enter the city name: ")
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=metric"
+
+    response = requests.get(url)
+
+    data = response.json()
+
+    # print(data)
+    if response.status_code == 200:
+
+        city = data["name"]
+        temperature= data["main"]["temp"]
+        humidity=data["main"]["humidity"]
+        weather=data["weather"][0]["main"]
+
+        print(f"City: {city}")
+        print(f"Temperature: {temperature}")
+        print(f"Humidity: {humidity}")
+        print(f"Weather: {weather}")
+
+        weather_record = {
+            "name":city,
+            "temperature": temperature,
+            "humidity": humidity,
+            "weather": weather
+        }
+
+        history = []
+
+        if os.path.exists("history.json"):
+            with open("history.json", "r") as file:
+                history = json.load(file)
+
+        history.append(weather_record)  
+
+
+        with open("history.json","w") as file:
+            json.dump(history,file,indent=4)
+
+        print("Data saved to history.json")  
+
+    else: 
+        print("Information not found.")         
+   
+
+
+
+
+
+        
